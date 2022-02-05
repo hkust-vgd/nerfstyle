074f1eb0a8f81c59caa080372dd8f312d0494a13
ferrophile <hwpang@cse.ust.hk>
2022-02-05 23:52:39 +0800

    fix pos enc fn

--- networks/embedder.py
+++ networks/embedder.py
@@ -9,12 +9,13 @@ class Embedder(nn.Module):
         super(Embedder, self).__init__()
         self.num_freqs = num_freqs
         self.append_input = append_input
-        self.register_buffer('basis', torch.tensor([pi * (2 ** i) for i in range(num_freqs)]))
+        self.register_buffer(
+            'basis', 2. ** torch.linspace(0., num_freqs - 1, steps=num_freqs))
 
     def forward(self, x: torch.Tensor):
         out = einops.repeat(x, 'n k -> n k c', c=self.num_freqs)
         out = out * self.basis
-        out = torch.stack([torch.sin(out), torch.cos(out)], dim=2)  # (n, k, 2, c)
+        out = torch.stack([torch.sin(out), torch.cos(out)], dim=2)  # (n,k,2,c)
         out = einops.rearrange(out, 'n k f c -> n (k c f)')
         if self.append_input:
             out = torch.cat([out, x], dim=1)
